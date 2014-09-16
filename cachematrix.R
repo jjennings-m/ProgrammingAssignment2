c8555f928621eafae5e5985a29c824f695a7e43b
jjennings-m <jjennings.m@gmail.com>
2014-09-16 04:32:30 -0500

    Syncing Changes
    Testing the GitHub sync / commit process.  Just added a comment to
    cashematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Testing the GitHub commit process
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
